3d1505b499d997c23fabecb332aa6e3e30aef16f
Pamela Dinarte <p.dinartech@gmail.com>
2022-05-29 21:41:39 -0600

    Update sintax.hs

--- sintax.hs
+++ sintax.hs
@@ -11,6 +11,26 @@ data Proposicion =
   | Equivalencia Proposicion Proposicion
   deriving Show
 
+(¬) :: Proposicion -> Proposicion
+(¬) prop = Negacion prop
+infix ¬
+
+(/\) :: Proposicion -> Proposicion -> Proposicion
+prop1 /\ prop2 = Conjuncion prop1 prop2
+infixl 7 /\
+
+(\/) :: Proposicion -> Proposicion -> Proposicion
+prop1 \/ prop2 = Disyuncion prop1 prop2
+infixl 6 \/
+
+(>>) :: Proposicion -> Proposicion -> Proposicion
+prop1 >> prop2 = Implicacion prop1 prop2
+infixr 5 >>
+
+(<=>) :: Proposicion -> Proposicion -> Proposicion
+prop1 <=> prop2 = Equivalencia prop1 prop2
+infixl 4 <=>
+
 -- ------------------------------------------------------------------
 p = Variable "p" 
 q = Variable "q" 
