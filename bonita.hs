1f72d3aa3d4a0fe65821209db4bdf842a8a92d9e
Pamela Dinarte <p.dinartech@gmail.com>
2022-06-03 22:48:57 -0600

    Update bonita.hs

--- bonita.hs
+++ bonita.hs
@@ -1,62 +1,45 @@
-import Sintax
+prioridad (Constante const)         = 6
+prioridad (Variable var)            = 6
+prioridad (Negacion prop)           = 5
+prioridad (Conjuncion prop1 prop2) = 4
+prioridad (Disyuncion prop1 prop2) = 3
+prioridad (Implicacion prop1 prop2) = 2
+prioridad (Equivalencia prop1 prop2) = 1
 
-prioridad prop =
-    case prop of
-        Constante False -> 6
-        Constante True -> 6
-        Variable nombre -> 6
-        Negacion prop1 -> 5
-        Conjuncion prop1 prop2 -> 4
-        Disyuncion prop1 prop2 -> 3
-        Implicacion prop1 prop2 -> 2
-        Equivalencia prop1 prop2 -> 1
 
-bonita prop =
-    case prop of
-        Constante False -> "false"
-        Constante True -> "true"
-        Variable nombre -> nombre
-        Negacion prop1 -> negacion_aux prop1
-        Conjuncion prop1 prop2 -> conjuncion_aux prop prop1 prop2
-        Disyuncion prop1 prop2 -> disyuncion_aux prop prop1 prop2
-        Implicacion prop1 prop2 -> implicacion_aux prop prop1 prop2
-        Equivalencia prop1 prop2 -> equivalencia_aux prop prop1 prop2
-    where
-        negacion_aux prop1 = "~" ++
-            if prioridad prop1 < 5 
-                then "(" ++ bonita prop1 ++ ")" 
-                else bonita prop1
-        conjuncion_aux prop prop1 prop2 =
-            if prioridad prop1 < prioridad prop
-                then  "(" ++ bonita prop1 ++ ")"
-                else bonita prop1
-            ++ "&&" ++
-            if prioridad prop2 > prioridad prop
-                then bonita prop2
-                else "(" ++ bonita prop2 ++ ")"
-        disyuncion_aux prop prop1 prop2 =
-            if prioridad prop1 < prioridad prop
-                then  "(" ++ bonita prop1 ++ ")"
-                else bonita prop1
-            ++ "||" ++
-            if prioridad prop2 > prioridad prop
-                then bonita prop2
-                else "(" ++ bonita prop2 ++ ")"
-        implicacion_aux prop prop1 prop2 =
-            if prioridad prop1 > prioridad prop
-                then  bonita prop1
-                else "(" ++ bonita prop1 ++ ")"
-            ++ "->" ++
-            if prioridad prop2 < prioridad prop
-                then "(" ++ bonita prop2 ++ ")"
-                else bonita prop2
-        equivalencia_aux prop prop1 prop2 = 
-            bonita prop1
-            ++ "<=>" ++
-            if prioridad prop2 > prioridad prop
-                then bonita prop2
-                else "(" ++ bonita prop2 ++ ")"
-{-
-main :: IO()
-main = print a where a = bonita prop1
--}
+isImpli (Implicacion p q) = True
+isImpli prop = False
+
+isEqui (Equivalencia p q) = True
+isEqui prop = False
+
+bonita (Constante const)         = if const then "True" else "False"
+bonita (Variable var)            = var
+bonita (Negacion prop)           = 
+  let 
+    priori = prioridad prop
+  in
+    "~" ++ (if priori < 5 then "(" ++ bonita prop ++ ")" else bonita prop)
+bonita prop = 
+  if isConj prop
+    then 
+      (if prioridad (getFirst prop) < prioridad prop then "(" ++ bonita (getFirst prop) ++ ")" else bonita (getFirst prop))
+      ++ " && " ++ 
+      (if prioridad (getSecond prop) > prioridad prop then bonita (getSecond prop) else "(" ++ bonita (getSecond prop) ++ ")")
+  else if isDisy prop
+    then
+      (if prioridad (getFirst prop) < prioridad prop then "(" ++ bonita (getFirst prop) ++ ")" else bonita (getFirst prop))    
+      ++ " || " ++
+      (if prioridad (getSecond prop) > prioridad prop then bonita (getSecond prop) else "(" ++ bonita (getSecond prop) ++ ")")
+  else if isImpli prop
+    then
+      (if prioridad (getFirst prop) > prioridad prop then bonita (getFirst prop) else "(" ++ bonita (getFirst prop) ++ ")")
+      ++ " => " ++
+      (if prioridad (getSecond prop) < prioridad prop then "(" ++ bonita (getSecond prop) ++ ")" else bonita (getSecond prop)) 
+  else if isEqui prop
+    then
+      bonita prop1 
+      ++ " <=> " ++ 
+      (if prioridad (getSecond prop) > prioridad prop then bonita (getSecond prop) else "(" ++ bonita (getSecond prop) ++ ")")
+  else
+    ""   
